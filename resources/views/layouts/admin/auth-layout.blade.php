4daaf30136c832bf85ab5a01f0f49b17198706b4
saltus-izumi <izumi@saltus.jp>
2026-01-06 16:51:58 +0900

    Livewareインストール

--- resources/views/layouts/admin/auth-layout.blade.php
+++ resources/views/layouts/admin/auth-layout.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
 </head>
 <body>
     <div class="tw:min-h-screen tw:flex">
@@ -49,5 +50,6 @@
             {{ $slot }}
         </div>
     </div>
+    @livewireScripts
 </body>
 </html>
