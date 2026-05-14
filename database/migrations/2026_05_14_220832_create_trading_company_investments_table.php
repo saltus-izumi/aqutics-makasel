49d964f6f0d7e3703243cfdf036ded2b63695b70
Takayuki Izumi <izumi@saltus.jp>
2026-05-14 22:12:10 +0900

    trading_company_investmentsテーブル作成

---
+++ database/migrations/2026_05_14_220832_create_trading_company_investments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trading_company_investments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('trading_company_id')->nullable()->comment('取引先会社ID');
+            $table->integer('investment_id')->nullable()->comment('物件ID');
+
+            $table->integer('created_user_id')->nullable()->comment('データ登録スタッフID');
+            $table->dateTime('user_created_at')->nullable()->comment('データ登録日時（ スタッフ）');
+            $table->integer('updated_user_id')->nullable()->comment('データ更新スタッフID');
+            $table->dateTime('user_updated_at')->nullable()->comment('データ更新日時（スタッフ）');
+            $table->integer('deleted_user_id')->nullable()->comment('データ削除スタッフID');
+            $table->dateTime('user_deleted_at')->nullable()->comment('データ削除日時（スタッフ）');
+            $table->dateTime('created_at')->nullable()->comment('データ登録日時');
+            $table->dateTime('updated_at')->nullable()->comment('データ更新日時');
+            $table->dateTime('deleted_at')->nullable()->comment('データ削除日時');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trading_company_investments');
+    }
+};
